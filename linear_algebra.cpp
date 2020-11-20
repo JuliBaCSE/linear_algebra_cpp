ae686ea7ec47400354ee81ca3c08ee8c6c1be464
jballetshofer <ge96fec@mytum.de>
2020-11-20 11:22:16 +0100

    added matrix vector multiplication

--- linear_algebra.cpp
+++ linear_algebra.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 
 
-void print_vec(const std::vector<int> & v)
+template <typename VarTypeVec>
+void printRowVec(const std::vector<VarTypeVec> & v)
 {
     std::cout << " [ ";
     for (const auto & elem : v)
@@ -12,7 +13,20 @@ void print_vec(const std::vector<int> & v)
     std::cout << "]" << std::endl;
 }
 
-void print_matrix(const std::vector<std::vector<int>> & A)
+template <typename VarTypeVec>
+void printColVec(const std::vector<VarTypeVec> & v)
+{
+    std::cout << " [ " <<std::endl;
+    for (const auto & elem : v)
+    {
+        std::cout << elem << std::endl;
+    }
+    std::cout << "]" << std::endl;
+}
+
+
+template <typename VarTypeMa>
+void printMatrix(const std::vector<std::vector<VarTypeMa> > & A)
 {
     std::cout << "[" << std::endl;
     for (const auto & row : A)
@@ -26,24 +40,22 @@ void print_matrix(const std::vector<std::vector<int>> & A)
     std::cout << "]" << std::endl;
 }
 
-int main() {
-
-    std::vector<std::vector<int>> A = { {1 , 2},
-                                        {3 , 4}};
-    std::vector<int> v = { 1 , 1};
-
+template<typename VarTypeMultResult>
+void vectorMatrixProduct(const std::vector<std::vector<VarTypeMultResult> > &A, const std::vector<VarTypeMultResult> &v){
     auto N = v.size();
 
     // Print the matrix A
     std::cout << "Matrix A is:" << std::endl;
-    print_matrix(A);
+    printMatrix(A);
 
     // print vector v
     std::cout << "Vector V is:" << std::endl;
-    print_vec(v);
+    printRowVec(v);
+
+
 
     // Multiply v * A and store it to a vector m
-    std::vector<int> m(N, 0);
+    std::vector<VarTypeMultResult> m(N, 0);
     for (auto i=0; i<N; ++i)
     {
         for (auto j=0; j<N; ++j)
@@ -51,10 +63,57 @@ int main() {
             m[j] += v[i]*A[i][j];
         }
     }
-
     // Print the vector m
     std::cout << "Vector m = v * A is:" << std::endl;
-    print_vec(m);
+    printVec(m);
+}
 
+template<typename VarType>
+std::vector<VarType> operator*(const std::vector<std::vector<VarType> > &A, const std::vector<VarType> &v){
+    
+    //check num col
+    
+    assert(A[0].size()== v.size());
+    std::vector<VarType>results;
+    VarType result = 0;
+    for (auto &row: A) {
+        result = 0;
+        unsigned int count= 0;
+		for (auto &e: row) {
+            result += e * v[count];
+            count++;
+		}
+        results.push_back(result);
+	}
+    return results;
+}
+
+//Test function
+bool testMatrixVectorProduct(const std::vector<int> &vec){
+    std::vector<int> reference = {1,-3};
+
+    for(auto i = 0; i<vec.size();i++){
+        if(vec[i]!= reference[i]){
+            std::cout << "There seems to be an error" << std::endl;
+            printColVec(reference);
+            printColVec(vec);
+
+            return false;
+        }
+    }
+    std::cout << "all fine";
+    return true;
+}
+
+int main() {
+
+    std::vector<std::vector<int> > A = { {1 , -1, 2},
+                                        {0 ,-3, 1}};
+    std::vector<int> v = { 2, 1,0};
+    //vectorMatrixProduct(A,v);
+
+    printColVec(A*v);
+
+    testMatrixVectorProduct(A*v);
     return 0;
 }
